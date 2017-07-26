a12cd4b9797592f47ab9e74e36790c9ac2ae3159
Eli Selkin <ejselkin@cpp.edu>
2017-07-25 19:52:02 -0700

    Initial Commit

---
+++ OpenLocationCode/OpenLocationCode.h
@@ -0,0 +1,19 @@
+//
+//  OpenLocationCode.h
+//  OpenLocationCode
+//
+//  Created by Eli Selkin on 7/25/17.
+//  Copyright © 2017 curbmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenLocationCode.
+FOUNDATION_EXPORT double OpenLocationCodeVersionNumber;
+
+//! Project version string for OpenLocationCode.
+FOUNDATION_EXPORT const unsigned char OpenLocationCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenLocationCode/PublicHeader.h>
+
+
